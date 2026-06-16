d3988157e8a47d3bac0a820f17cd55f29f90d942
tara <losronda2000@gmail.com>
2026-06-16 04:48:08 -0400

    Add SHEETS object with Google Sheets URLs

--- configt.js
+++ configt.js
@@ -1,1 +1,8 @@
-
+const SHEETS = {
+  songs: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTxNCkmos8f9p_UzZMlNiXuzs91NgFMQ9dFcbDrq5ApIoR16sISi6mHpaTsdLxX8Dgx0pMd6Mna-VP0/pub?gid=106245926&single=true&output=csv",
+  albums: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTxNCkmos8f9p_UzZMlNiXuzs91NgFMQ9dFcbDrq5ApIoR16sISi6mHpaTsdLxX8Dgx0pMd6Mna-VP0/pub?gid=1756584314&single=true&output=csv",
+  videos: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTxNCkmos8f9p_UzZMlNiXuzs91NgFMQ9dFcbDrq5ApIoR16sISi6mHpaTsdLxX8Dgx0pMd6Mna-VP0/pub?gid=782049017&single=true&output=csv",
+  streaming: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTxNCkmos8f9p_UzZMlNiXuzs91NgFMQ9dFcbDrq5ApIoR16sISi6mHpaTsdLxX8Dgx0pMd6Mna-VP0/pub?gid=1690200732&single=true&output=csv",
+  sales: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTxNCkmos8f9p_UzZMlNiXuzs91NgFMQ9dFcbDrq5ApIoR16sISi6mHpaTsdLxX8Dgx0pMd6Mna-VP0/pub?gid=1786310088&single=true&output=csv",
+  radio: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTxNCkmos8f9p_UzZMlNiXuzs91NgFMQ9dFcbDrq5ApIoR16sISi6mHpaTsdLxX8Dgx0pMd6Mna-VP0/pub?gid=1084335573&single=true&output=csv"
+};
