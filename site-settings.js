2365a62a6e5d32e5504f511d448595625d9c259e
tara <losronda2000@gmail.com>
2026-06-17 23:17:12 -0400

    Add site settings for Sweet 16 Charts

---
+++ site-settings.js
@@ -0,0 +1,8 @@
+window.SWEET16_SETTINGS = {
+  "siteTitle": "Sweet 16 Charts",
+  "siteTagline": "The official weekly music chart",
+  "newsTitle": "Sweet 16 News",
+  "newsBody": "Latest chart news will appear here.",
+  "newsImage": "",
+  "accentColor": ""
+};
