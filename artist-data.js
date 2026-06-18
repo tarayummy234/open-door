efd8e5e4d35d84fc3451a9d1d3b2a9591dd18df8
tara <losronda2000@gmail.com>
2026-06-17 20:48:25 -0400

    Initialize artist-data.js with example data
    Added example artist data structure for SWEET16.

---
+++ artist-data.js
@@ -0,0 +1,16 @@
+window.SWEET16_ARTIST_DATA = {
+  /*
+  Example:
+
+  "Ariana Grande": {
+    banner: "https://www.rollingstone.com/wp-content/uploads/2026/05/ariana-grande-petal-new-details.jpg?w=1581&h=1054&crop=1",
+    image: "https://images.genius.com/8e1cc3cabafaa6db3d7e5d8ef02f9e5e.1000x1000x1.png",
+    subtitle: "Pop artist",
+    bio: "Ariana Grande is one of the biggest artists on Sweet 16 Charts with over 6 #1's & over 20 top 10's.",
+    facts: [
+      "Longest Running #1 Song |8 Weeks|",
+      "Biggest Audience on Radio Ever |+110M|"
+    ]
+  }
+  */
+};
